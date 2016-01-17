14b332cbfc022a82e6370ad414a7d648403220f5
Michał Ferchow <deseteral@gmail.com>
2016-01-17 13:00:50 +0100

    Map is kind of working
    But there's still a lot to be done to make it feel better

--- OpenFSRC/Content/page.js
+++ OpenFSRC/Content/page.js
@@ -1,6 +1,19 @@
 var connection = new WebSocket('ws://localhost:8958');
+var app = null;
 
-connection.onmessage = function(e) {
-  var data = JSON.parse(e.data);
-  console.log(data);
-};
+window.addEventListener('DOMContentLoaded', function() {
+  app = document.querySelector('#app');
+
+  app.data = {
+    PositionInformation: {
+      Latitude: 0,
+      Longitude: 0,
+      Altitude: 0
+    }
+  };
+
+  connection.onmessage = function(e) {
+    app.data = JSON.parse(e.data);
+    console.log(app.data);
+  };
+});
